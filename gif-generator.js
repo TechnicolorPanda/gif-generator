cc84b3dafe72e3f23e4b969b867209be30644ef8
LorriAnn <LorriAnn7@att.net>
2021-01-04 21:38:21 -0500

    added search function and error result

--- gif-generator.js
+++ gif-generator.js
@@ -1,25 +1,56 @@
-function getGif() {
+// displays image of bunny upon failure to connect
+
+function errorImage() {
   const img = document.querySelector('img');
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=CEm2z9suJ31Eb7jB6G3ZBWDSND8HOECl&s=animals', {
+  img.src = 'bunny.jpg';
+}
+
+// retrieve another animal picture upon selecting 'More Animals' button
+
+function refreshButton() {
+  const refresh = document.getElementById('reset');
+  refresh.addEventListener('click', getGif);
+}
+
+// retrieve defined gif
+
+function searchForGif(searchTerm) {
+  const img = document.querySelector('img');
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=CEm2z9suJ31Eb7jB6G3ZBWDSND8HOECl&s=${searchTerm}`, {
     mode: 'cors',
   })
     .then((response) => response.json())
     .then((response) => {
       img.src = response.data.images.original.url;
     })
-    .catch((response) => {
-      console.log('error');
+    .catch(() => {
+      errorImage();
     });
+}
+
+// retrieve input of desired search
+
+function defineSearch() {
+  const searchTerm = document.getElementById('search').value;
+  searchForGif(searchTerm);
+}
+
+// selects submit form
+
+function selectSubmit() {
+  const submitButton = document.getElementById('submit');
+  submitButton.addEventListener('click', () => {
+    defineSearch();
+  });
+}
+
+// activate initial search and inputs
+
+function getGif() {
+  const searchTerm = 'animals';
+  searchForGif(searchTerm);
   refreshButton();
-}
-
-function refreshButton() {
-  const refresh = document.getElementById('reset');
-  refresh.addEventListener('click', refreshPage);
-}
-
-function refreshPage() {
-  getGif();
+  selectSubmit();
 }
 
 getGif();
